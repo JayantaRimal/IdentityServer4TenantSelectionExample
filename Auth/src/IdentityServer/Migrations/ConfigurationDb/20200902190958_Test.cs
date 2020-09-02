2f0c8ff6733338b2b826afdf4686e7f94b9e555e
Jayanta Rimal <rimaljayant@gmail.com>
2020-09-03 00:59:06 +0545

    Initial Commit

---
+++ Auth/src/IdentityServer/Migrations/ConfigurationDb/20200902190958_Test.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Migrations.ConfigurationDb
+{
+    public partial class Test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ApiResources",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 510, DateTimeKind.Utc).AddTicks(797));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecrets",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 513, DateTimeKind.Utc).AddTicks(9349));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecrets",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 520, DateTimeKind.Utc).AddTicks(6838));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecrets",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 520, DateTimeKind.Utc).AddTicks(7160));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 512, DateTimeKind.Utc).AddTicks(6016));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 513, DateTimeKind.Utc).AddTicks(856));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 513, DateTimeKind.Utc).AddTicks(862));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 513, DateTimeKind.Utc).AddTicks(864));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResources",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 512, DateTimeKind.Utc).AddTicks(1486));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResources",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 512, DateTimeKind.Utc).AddTicks(2709));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResources",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 19, 9, 57, 512, DateTimeKind.Utc).AddTicks(2713));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "ApiResources",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 413, DateTimeKind.Utc).AddTicks(8785));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecrets",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 422, DateTimeKind.Utc).AddTicks(8137));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecrets",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 430, DateTimeKind.Utc).AddTicks(6893));
+
+            migrationBuilder.UpdateData(
+                table: "ClientSecrets",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 430, DateTimeKind.Utc).AddTicks(7307));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 419, DateTimeKind.Utc).AddTicks(7103));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 420, DateTimeKind.Utc).AddTicks(7173));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 420, DateTimeKind.Utc).AddTicks(7192));
+
+            migrationBuilder.UpdateData(
+                table: "Clients",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 420, DateTimeKind.Utc).AddTicks(7201));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResources",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 418, DateTimeKind.Utc).AddTicks(5812));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResources",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 418, DateTimeKind.Utc).AddTicks(9248));
+
+            migrationBuilder.UpdateData(
+                table: "IdentityResources",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "Created",
+                value: new DateTime(2020, 9, 2, 11, 59, 15, 418, DateTimeKind.Utc).AddTicks(9252));
+        }
+    }
+}
---
+++ Auth/src/IdentityServer/Migrations/MasterDb/20200902115835_Master.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace IdentityServer.Migrations.MasterDb
+{
+    public partial class Master : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Companies",
+                columns: table => new
+                {
+                    Id = table.Column<long>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CompanyName = table.Column<string>(nullable: true),
+                    UserId = table.Column<string>(nullable: true),
+                    DatabaseName = table.Column<string>(nullable: true),
+                    StartDate = table.Column<DateTime>(nullable: false),
+                    EndDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Companies", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Companies",
+                columns: new[] { "Id", "CompanyName", "DatabaseName", "EndDate", "StartDate", "UserId" },
+                values: new object[] { 1L, "jayanta", "JayantaDatabase", new DateTime(2020, 9, 2, 17, 43, 34, 579, DateTimeKind.Local).AddTicks(2954), new DateTime(2020, 9, 2, 17, 43, 34, 577, DateTimeKind.Local).AddTicks(6959), "1" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Companies");
+        }
+    }
+}
